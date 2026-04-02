9a526536f47d8174e081d4c90d4845b6ad0ea069
Jairus Otiko <jairusotiko@Jairuss-MacBook-Air.local>
2026-04-02 16:37:08 +0300

    chore:Added txt

---
+++ .next/server/app/957af49c9f40e16bd1c06476137d6bd88b7b06fcc672a6caa0deeeb65509154f/route_client-reference-manifest.js
@@ -0,0 +1,2 @@
+globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
+globalThis.__RSC_MANIFEST["/957af49c9f40e16bd1c06476137d6bd88b7b06fcc672a6caa0deeeb65509154f/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
